e8e3d6bbb678f914e58e69539f88d88175bd0892
Seannas9912 <seanrussell9099@gmail.com>
2025-06-16 10:52:42 +0200

    Lesson 2

---
+++ strings.py
@@ -0,0 +1,18 @@
+#Strings
+
+message1 = 'Hello, World!'
+
+print(message1)
+
+message2 = "Hello, World! (With double quotes)"
+
+print(message2)
+
+message3 = "Bob's Burgers(With single quotes inside double quotes)"
+
+print(message3)
+
+message4 = """This is a multi-line string.
+It can span multiple lines and include 'single quotes' and "double quotes" without escaping them."""
+
+print(message4)
