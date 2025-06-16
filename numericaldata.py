5fff7ab4f6f25608d23936596829d40e615a2902
Seannas9912 <seanrussell9099@gmail.com>
2025-06-16 11:37:30 +0200

    Lesson 4

---
+++ numericaldata.py
@@ -0,0 +1,49 @@
+#Numerical Data
+
+#integer is a whole number
+#float is a decimal number
+#complex is a number with a real and imaginary part
+#boolean is a True or False value
+#None is a null value
+#list is a collection of items
+#tuple is an immutable collection of items
+#dictionary is a collection of key-value pairs
+#set is a collection of unique items
+#frozenset is an immutable set
+#bytes is a sequence of bytes
+#bytearray is a mutable sequence of bytes
+#memoryview is a view of a memory buffer
+#range is a sequence of numbers
+#slice is a range of indices
+#decimal is a fixed-point or floating-point number with a fixed number of decimal places
+#fraction is a rational number represented by a numerator and denominator
+#datetime is a date and time object
+#timedelta is a duration of time
+#decimal.Decimal is a fixed-point or floating-point number with a fixed number of decimal places
+#fractions.Fraction is a rational number represented by a numerator and denominator
+#numpy.ndarray is a multidimensional array
+#pandas.Series is a one-dimensional labeled array
+#pandas.DataFrame is a two-dimensional labeled data structure
+#numpy is a library for numerical computing
+#pandas is a library for data analysis and manipulation
+
+num1 = 3 # integer number but C# written as int num1 = 3;
+num2 = 3.14 # float number but C# written as float num2 = 3.14f;
+num3 = 1 + 2 #will be added as 3 without complex part j
+num4 = 1 + 2j  # complex number with real and imaginary parts
+
+print(num1)
+print(type(num1))
+# Output: <class 'int'>
+
+print(num2)
+print(type(num2))
+# Output: <class 'float'>
+
+print(num3)
+print(type(num3))
+# Output: <class 'int'>
+
+print(num4)
+print(type(num4))
+# Output: <class 'complex'>
